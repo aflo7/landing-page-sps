32514d3284c373f4873b088febb070d0dd0bdf7e
Andres Flores <andresflo883@gmail.com>
2023-12-12 11:51:39 -0500

    finished the site styling and html

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="nav-wrapper">
@@ -16,75 +11,103 @@ function App() {
               src="https://www.spscommerce.com/wp-content/uploads/2018/07/logo-white.svg"
               id="spsIcon"
             />
-            {/* <div>Products</div>
-        <div>Partners</div>
-        <div>Resources</div>
-        <div>Company</div>
-        <div>Current Customer</div> */}
+            <div className="nav-button-div">
+              <div>Javascript</div>
+              <div>HTML</div>
+              <div>CSS</div>
+              <div>Remade By</div>
+              <div>Andres</div>
+              <button id="contactBtn">Contact</button>
+
+            </div>
             <GiHamburgerMenu size="1.6rem" id="giHamburgerMenu" />
           </div>
           <div className="second-nav">
-            <h1>SPS Commerce Offers Retail Solutions</h1>
+            <h1>SPS Commerce Mockup Site</h1>
             <p>
-              The SPS Retail Network powers the retail supply chain around the
-              globe. Our solutions connect trading partners, accelerate
-              products-to-shelf and allow for the collaboration to grow sales
-              with data insights. We ensure every retail channel works together
-              to meet the consumer where they want to buy.
+              I made this replica of the SPS website to show off my web dev
+              skills, The site is responsive for mobile and desktop devices. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Iure, dolorem! Et deserunt ipsa optio nemo similique. Nesciunt,
+              voluptatum? Nemo quo dolore veritatis, nulla debitis eligendi
+              architecto unde laboriosam earum ratione!
             </p>
-            <button id="whyChooseSpsBtn">Why Choose SPS</button>
-            <button id="referredToSpsBtn">Referred to SPS</button>
+            <div className="second-nav-button-wrapper">
+              <button id="whyChooseSpsBtn">Why Choose SPS</button>
+              <button id="referredToSpsBtn">Referred to SPS</button>
+            </div>
           </div>
         </nav>
       </div>
       <main>
-        <h2>SPS Commerce Solutions to Solve Your Supply Chain Challenges</h2>
-        <p>
-          Our retail solutions automate the exchange of data about products,
-          inventory, orders, shipments, payments and sales. More than 115,000
-          companies around the globe rely on SPS to make their trading partner
-          relationships more collaborative and profitable. View our products at
-          a glance below:
-        </p>
-        <div className="icon-and-text-wrapper">
-          <img
-            src="https://www.spscommerce.com/wp-content/uploads/2015/01/icon-fulfillment.png"
-            id="iconFulfillment"
-          />
-          <div className="find-an-edi-solution-text">Find an EDI Solution</div>
-          <p>
-            Solve your order fulfillment problems, gain EDI capability, attain
-            EDI compliance, connect systems and onboard trading partners with
-            SPS Fulfillment EDI.
-          </p>
-          <a>Learn More</a>
+        <div className="top-main">
+          <h2>SPS Commerce Solutions to Solve Your Supply Chain Challenges</h2>
+          <div>
+            Our retail solutions automate the exchange of data about products,
+            inventory, orders, shipments, payments and sales. More than 115,000
+            companies around the globe rely on SPS to make their trading partner
+            relationships more collaborative and profitable. View our products
+            at a glance below:
+          </div>
         </div>
-        <div className="icon-and-text-wrapper">
-          <img
-            src="https://www.spscommerce.com/wp-content/uploads/2015/01/icon-fulfillment.png"
-            id="iconFulfillment"
-          />
-          <div className="find-an-edi-solution-text">Find an EDI Solution</div>
-          <p>
-            Solve your order fulfillment problems, gain EDI capability, attain
-            EDI compliance, connect systems and onboard trading partners with
-            SPS Fulfillment EDI.
-          </p>
-          <a>Learn More</a>
-        </div>
+        <div className="icons-and-text-wrapper">
+          <div className="icon-and-text-wrapper">
+            <div>
+              <img
+                src="https://www.spscommerce.com/wp-content/uploads/2015/01/icon-fulfillment.png"
+                id="iconFulfillment"
+              />
+            </div>
+            <div>
+              <div className="find-an-edi-solution-text">
+                Find an EDI Solution
+              </div>
+              <p>
+                Solve your order fulfillment problems, gain EDI capability,
+                attain EDI compliance, connect systems and onboard trading
+                partners with SPS Fulfillment EDI.
+              </p>
+              <a>Learn More</a>
+            </div>
+          </div>
+          <div className="icon-and-text-wrapper">
+            <div>
+              <img
+                src="https://www.spscommerce.com/wp-content/uploads/2015/01/icon-fulfillment.png"
+                id="iconFulfillment"
+              />
+            </div>
+            <div>
+              <div className="find-an-edi-solution-text">
+                Find an EDI Solution
+              </div>
+              <p>
+                Solve your order fulfillment problems, gain EDI capability,
+                attain EDI compliance, connect systems and onboard trading
+                partners with SPS Fulfillment EDI.
+              </p>
+              <a>Learn More</a>
+            </div>
+          </div>
 
-        <div className="icon-and-text-wrapper">
-          <img
-            src="https://www.spscommerce.com/wp-content/uploads/2015/01/icon-fulfillment.png"
-            id="iconFulfillment"
-          />
-          <div className="find-an-edi-solution-text">Find an EDI Solution</div>
-          <p>
-            Solve your order fulfillment problems, gain EDI capability, attain
-            EDI compliance, connect systems and onboard trading partners with
-            SPS Fulfillment EDI.
-          </p>
-          <a>Learn More</a>
+          <div className="icon-and-text-wrapper">
+            <div>
+              <img
+                src="https://www.spscommerce.com/wp-content/uploads/2015/01/icon-fulfillment.png"
+                id="iconFulfillment"
+              />
+            </div>
+            <div>
+              <div className="find-an-edi-solution-text">
+                Find an EDI Solution
+              </div>
+              <p>
+                Solve your order fulfillment problems, gain EDI capability,
+                attain EDI compliance, connect systems and onboard trading
+                partners with SPS Fulfillment EDI.
+              </p>
+              <a>Learn More</a>
+            </div>
+          </div>
         </div>
       </main>
     </>
